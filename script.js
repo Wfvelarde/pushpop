62e77376cfcbb3e9d9ab8ebb27f0ad0a93eec212
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2016-12-09 11:16:48 -0700

    added accumulator for collection

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function (){
   console.log("welcome ! ");
-  var collection = [];
-})
+  var collection = localStorage;
+});
 
 
 $("#trigger").on("click", function(){
   console.log("button has been clicked !");
-})
+  collection.push($('#add_stuff').val);
+});
